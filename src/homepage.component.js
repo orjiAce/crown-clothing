bbea8ff8f10cc9c2a8698158e68b3b51fc101d25
orjiAce <orjiace@gmail.com>
2020-01-22 09:24:13 +0100

    second commit

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -5,8 +5,64 @@ import './homepage.styles.scss'
 
  function Homepage() {
     return (
-        <div>
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            HATS
+                        </h1>
+                        <span>
+            SHOP NOW
+        </span>
+                    </div>
 
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            JACKETS
+                        </h1>
+                        <span>
+            SHOP NOW
+        </span>
+                    </div>
+
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            SNEAKERS
+                        </h1>
+                        <span>
+            SHOP NOW
+        </span>
+                    </div>
+
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            WOMENS
+                        </h1>
+                        <span>
+            SHOP NOW
+        </span>
+                    </div>
+
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">
+                            MENS
+                        </h1>
+                        <span>
+            SHOP NOW
+        </span>
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
